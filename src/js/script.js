91de446b59d78c837167a07d7fe006ebf85bb12c
Jakub Kalinowski <kalj@netcompany.com>
2023-09-19 18:24:22 +0200

    Calling API to get a list of players

---
+++ src/js/script.js
@@ -0,0 +1,97 @@
+// const response = {
+//     "pokedata": [
+//         {"Name":"Adam Abramczyk","Goals":"21","Assists":"14","YellowCards":"0","RedCards":"0","MOTMS":"1"},
+//         {"Name":"Artur Abramczyk","Goals":"5","Assists":"3","YellowCards":"1","RedCards":"0","MOTMS":"0"}
+//      ]
+//  }
+const endpointUrl =
+  "https://ligabemowskafunctionsapp.azurewebsites.net/api/GetAllPlayers?";
+
+//var response;
+var tableContent;
+var tableButtons;
+
+const createRow = (obj) => {
+  const row = document.createElement("tr");
+  const objKeys = Object.keys(obj);
+  objKeys.map((key) => {
+    const cell = document.createElement("td");
+    cell.setAttribute("data-attr", key);
+    cell.innerHTML = obj[key];
+    row.appendChild(cell);
+  });
+  return row;
+};
+
+const getTableContent = (data) => {
+  data.map((obj) => {
+    const row = createRow(obj);
+    tableContent.appendChild(row);
+  });
+};
+
+const sortData = (data, param, direction = "asc") => {
+  tableContent.innerHTML = "";
+  const sortedData =
+    direction == "asc"
+      ? [...data].sort(function (a, b) {
+          if (a[param] < b[param]) {
+            return -1;
+          }
+          if (a[param] > b[param]) {
+            return 1;
+          }
+          return 0;
+        })
+      : [...data].sort(function (a, b) {
+          if (b[param] < a[param]) {
+            return -1;
+          }
+          if (b[param] > a[param]) {
+            return 1;
+          }
+          return 0;
+        });
+
+  getTableContent(sortedData);
+};
+
+const resetButtons = (event) => {
+  [...tableButtons].map((button) => {
+    if (button !== event.target) {
+      button.removeAttribute("data-dir");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  tableContent = document.getElementById("table-content");
+  tableButtons = document.querySelectorAll("th button");
+
+  fetch(endpointUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      getTableContent(data);
+
+      [...tableButtons].map((button) => {
+        button.addEventListener("click", (e) => {
+          resetButtons(e);
+          if (e.target.getAttribute("data-dir") == "desc") {
+            sortData(data, e.target.id, "desc");
+            e.target.setAttribute("data-dir", "asc");
+          } else {
+            sortData(data, e.target.id, "asc");
+            e.target.setAttribute("data-dir", "desc");
+          }
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+});
